145cbcc003e8741cc57c21cefc68f94b3cf51e3d
Rayitz <67914411+Rayitz@users.noreply.github.com>
2020-11-16 10:12:49 -0600

    Transformers with the same name
    Functionality to create more than one transformer with the same name

--- src/main/java/com/raymundo/aequilibrium/api/TransformerRepository.java
+++ src/main/java/com/raymundo/aequilibrium/api/TransformerRepository.java
@@ -6,7 +6,7 @@ import com.raymundo.aequilibrium.model.Transformer;
 
 public interface TransformerRepository extends JpaRepository<Transformer, Long> {
 
-	public Transformer getTransformerByName(String name);
+//	public Transformer getTransformerByName(String name);
 	
 
 }
